c025e81c0cb6342407313b0137075a6b405abf22
quol <kuolamol77@gmail.com>
2025-05-02 12:18:39 +0300

    added the week seven assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const animateBtn = document.getElementById("animateBtn");
+
+animateBtn.addEventListener("click", function () {
+    const box = document.getElementById("box");
+    box.classList.toggle("animate");
+  });
